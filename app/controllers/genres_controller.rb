dbd475c173b6e462ea4491d74346613db7c2c9d0
Oscar Vasquez <odvperaza@ucm.es>
2015-06-07 20:54:13 +0200

    limpiando genres controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,4 @@
 class GenresController < ApplicationController
-  require 'echonest-ruby-api'
-  skip_before_filter :getArtist  
    # GET /genres
   # GET /genres.json
   def index
@@ -23,16 +21,6 @@ class GenresController < ApplicationController
   # GET /genres/1/edit
   def edit
 
-  end
-
-  def aaa 
-    
-    respond_to do |format|
-      format.html # contact.html.erb
-      format.json {
-        render :response => {:par => par}
-      }
-    end
   end
 
   # POST /genres
@@ -74,46 +62,6 @@ class GenresController < ApplicationController
       format.json { head :no_content }
     end
   end
-
-  def getArtist 
-    genr = params [:gen]
-    respond_to do |format|
-      format.html # contact.html.erb
-      format.json {
-        render :response => {:gen => genr}
-      }
-    end
-    lista_generos = []
-    genr.each do |generisimo|
-      lista_generos.push (generisimo)
-      similar = Echonest::Genre.similar('S6YKWF6QCDG012BBJ', generisimo, result: 5)
-      g = similar.to_json
-      json = JSON.parse (g)
-      json['genres'].each do |genress| 
-        genr.push (genress['name'])
-      end
-    end
-    lista_artistas = []
-    lista_generos.each do |genero|
-      artist = Echonest::Genre.artist('S6YKWF6QCDG012BBJ', genero, result: 10)
-      g = artist.to_json
-      json = JSON.parse (g)
-      json['artists'].each do |a| 
-        lista_artistas << a['id']
-      end
-    end
-    lista_canciones = []
-    lista_artistas.each do |artista|
-      song = Echonest::Artist.songs('S6YKWF6QCDG012BBJ', artista, result: 3)
-      g = artist.to_json
-      json = JSON.parse (g)
-      json['songs'].each do |a| 
-      lista_canciones << a['id']
-      end
-    end
-
-  end
-
 
   private
     # Use callbacks to share common setup or constraints between actions.
